68df766781d39d90f49dc90072da57543a866371
GaneshByte1 <ganeshbytte@gmaigit config --global user.email ganeshbyte@gmail.com>
2025-09-26 23:39:34 +0530

    Arrays and basic question solve

---
+++ Arrays/PairsArrays.java
@@ -0,0 +1,21 @@
+public class PairsArrays {
+
+    public static void pairs_of_array(int numbers[]){
+
+        for(int i = 0;i<numbers.length;i++){
+            int current = numbers[i];
+            for(int j = i+1;j < numbers.length;j++){
+                System.out.print("(" + current + " , " + numbers[j] + ")");
+            }
+            System.out.println();
+        }
+        
+    }
+    public static void main(String[] args) {
+        int numbers[] = {2 , 4 , 6 , 8 , 10};
+        pairs_of_array(numbers);
+        System.out.println();
+
+        
+    }
+}
---
+++ basics/FizzBuzz.java
@@ -0,0 +1,21 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        int numbers = 30;
+        for(int i = 1;i<=numbers;i++){
+
+            if(i % 3 == 0 && i % 5 ==0){
+                System.out.println("FizzBuzz");
+            }
+            else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }
+            else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }else{
+                System.out.println(i);
+            }
+        }
+        System.out.println();
+            
+    }
+}
